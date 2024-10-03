9fce9f8c7f8388ea08bfa7cab8b8257a600cc89f
Pablo Scherpa <desarrollador@pabloscherpa.com.ar>
2024-10-03 20:09:14 -0300

    first styles to the layout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,29 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import Logo from "../components/logo";
 
 export default function AppLayout() {
   return (
     <>
-        <div>AppLayout</div>
-        <Outlet />    
+      <header className="bg-gray-800 py-5">
+        <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+          <div className="w-64">
+            <Logo />
+          </div>
+        </div>
+      </header>
+
+      <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+        <Outlet />
+      </section>
+
+      <footer className="py-5">
+        <p className="text-center">
+          &copy; {new Date().getFullYear()} Todos los derechos reservados -{" "}
+          <span className="font-bold">
+            <a href="https://estudiols.net.ar">Estudio LS</a>
+          </span>
+        </p>
+      </footer>
     </>
-  )
+  );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AppLayout from './layouts/AppLayout'
-import DashboardView from './views/DashboardView'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import DashboardView from "./views/DashboardView";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={ <AppLayout /> }>
-            <Route path='/' element={ <DashboardView /> } index />
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<DashboardView />} index />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
